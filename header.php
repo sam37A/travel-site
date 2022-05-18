f213e254864674bc3423349705132b643a6762b1
sam37A <sahilsan30@gmail.com>
2022-05-18 15:08:20 -0700

    nav and logo is set up

--- header.php
+++ header.php
@@ -29,20 +29,8 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$travel_site_description = get_bloginfo( 'description', 'display' );
-			if ( $travel_site_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $travel_site_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			?>
+				
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
