2a92eceaf0bd4b274f0efee0f7084651a11eb674
sam37A <sahilsan30@gmail.com>
2022-07-05 21:49:14 -0700

    code before turning into the php

--- template-parts/info-section.php
+++ template-parts/info-section.php
@@ -4,10 +4,8 @@
             <img src="<?php the_field('info_image');?>" alt="beach chairs with umberellas on a beach." height="490px" width="50%" style="border: 1px solid #000;">
         </div>
         <div class="infoBox infoTextbox">
-            <h2>BOOK YOUR DREAM VACATION TODAY</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius hic voluptatibus vel cum nisi nulla deleniti aspernatur doloremque libero dolor. Earum explicabo libero atque neque architecto perspiciatis aliquam quas est.
-            Magnam laborum sed dolorem non maxime placeat dolore possimus nisi architecto mollitia, officia harum atque soluta laboriosam voluptas tenetur aliquid dignissimos, dicta minus repellat quis. Illum fugit suscipit molestiae doloremque.
-            Fuga earum totam harum unde asperiores vitae at adipisci in reprehenderit debitis, quo nisi? Architecto vitae voluptatibus maiores! Sunt vitae nulla nostrum repudiandae dignissimos dolorum deserunt, expedita minima veritatis cupiditate?
+            <h2> <?php the_field('info_box_title');?></h2>
+            <p> <?php the_field('infobox_paragraph');?>
            </p>
         </div>
     </section>
